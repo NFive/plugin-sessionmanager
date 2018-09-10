73715c127d9c02f5ba5f930ba6526a9829e26bba
Joe Biellik <joebiellik@gmail.com>
2018-09-10 01:35:00 +0100

    Update SDK and config

--- Configuration.cs
+++ Configuration.cs
@@ -6,9 +6,9 @@ using NFive.SDK.Core.Controllers;
 namespace NFive.SessionManager
 {
 	[PublicAPI]
-	public class Configuration : IControllerConfiguration
+	public class Configuration : ControllerConfiguration
 	{
-		private Lazy<ushort> maxClients = new Lazy<ushort>(() => (ushort)API.GetConvarInt("sv_maxclients", 32));
+		private readonly Lazy<ushort> maxClients = new Lazy<ushort>(() => (ushort)API.GetConvarInt("sv_maxclients", 32));
 
 		public uint ConnectionTimeout { get; set; } = 60000;
 		public uint ReconnectGrace { get; set; } = 120000;
